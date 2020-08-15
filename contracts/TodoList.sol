b2506e47c83006b940ec0353cf9da4f16ba95e7e
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-08-15 13:17:05 +0545

    List all the task in web application

---
+++ contracts/TodoList.sol
@@ -0,0 +1,30 @@
+// Smart Contract for Todo-List 
+
+// 1. Version of solidity Programming language that we want to use
+pragma solidity ^0.5.0;
+
+// 2. Declare the smart contract 
+contract TodoList {
+  uint public taskCount = 0; //State 
+
+  struct Task {
+    uint id;
+    string content;
+    bool  completed;
+  }
+
+  // Storage 
+  mapping(uint => Task) public tasks; 
+
+  // Constructor fn to initilize the task
+  constructor() public {
+    createTask("Complete BCT Notes website new features");
+    createTask("Complete pending work");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
+
+} 
---
+++ src/app.js
@@ -0,0 +1,124 @@
+App = {
+  loading: false,
+  contracts: {},
+
+  load: async () => {
+    // load the application
+    console.log("app loading ...");
+    await App.loadWeb3();
+    await App.loadAccount();
+    await App.loadContract();
+    await App.render();
+  },
+  // alert hurry moon journey trial twist affair fatal pilot normal dose actor
+  // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
+  loadWeb3: async () => {
+    if (typeof web3 !== "undefined") {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      window.alert("Please connect to Metamask.");
+    }
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      window.web3 = new Web3(ethereum);
+      try {
+        // Request account access if needed
+        await ethereum.enable();
+        // Acccounts now exposed
+        web3.eth.sendTransaction({
+          /* ... */
+        });
+      } catch (error) {
+        // User denied account access...
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = web3.currentProvider;
+      window.web3 = new Web3(web3.currentProvider);
+      // Acccounts always exposed
+      web3.eth.sendTransaction({
+        /* ... */
+      });
+    }
+    // Non-dapp browsers...
+    else {
+      console.log(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  },
+
+  loadAccount: async () => {
+    App.account = web3.eth.accounts[0];
+  },
+
+  loadContract: async () => {
+    const todoList = await $.getJSON("TodoList.json");
+    App.contracts.TodoList = TruffleContract(todoList);
+    App.contracts.TodoList.setProvider(App.web3Provider);
+
+    App.todoList = await App.contracts.TodoList.deployed();
+  },
+  render: async () => {
+    if (App.loading) {
+      return;
+    }
+
+    $("#account").html(App.account);
+    // Render Task
+    await App.renderTask();
+
+    App.setLoading(false);
+  },
+
+  renderTask: async () => {
+    // Load the total task count from the blockchain
+    const taskCount = await App.todoList.taskCount();
+    // Render out each task with a new template
+    const $taskTemplate = $(".taskTemplate");
+    for (let i = 1; i <= taskCount; i++) {
+      // Fetch the task data from the blockchain
+      const task = await App.todoList.tasks(i);
+      const taskId = task[0].toNumber();
+      const taskContent = task[1];
+      const taskCompleted = task[2];
+      // TASK HTML
+      const $newTaskTemplate = $taskTemplate.clone();
+      $newTaskTemplate.find(".content").html(taskContent);
+      $newTaskTemplate
+        .find("input")
+        .prop("name", taskId)
+        .prop("checked", taskCompleted)
+        .on("click", App.toggleComplete);
+      // Filter task acc to status
+      if (taskCompleted) {
+        $("#completedTaskList").append($newTaskTemplate);
+      } else {
+        $("#taskList").append($newTaskTemplate);
+      }
+      // Show the task
+      $newTaskTemplate.show();
+    }
+  },
+
+  setLoading: (boolean) => {
+    App.loading = boolean;
+    const loader = $("#loader");
+    const content = $("#content");
+    if (boolean) {
+      loader.show();
+      content.hide();
+    } else {
+      loader.hide();
+      content.show();
+    }
+  },
+};
+
+$(() => {
+  $(window).load(() => {
+    App.load();
+  });
+});
